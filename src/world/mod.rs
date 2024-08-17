17fcae593295e8282e84a37f1c15cdc068b5ea47
Florian Depraz <florian.depraz@outlook.com>
2024-08-17 21:54:40 +0200

    Fix additional line of dirt
    Signed-off-by: Florian Depraz <florian.depraz@outlook.com>

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -2,17 +2,21 @@
  * Copyright (c) 2024 Elieva Pignat, Florian Depraz, Louis Mayencourt
  */
 
-use bevy::{color::palettes::css::GREEN, prelude::*};
+use std::isize;
+
+use bevy::prelude::*;
 use rand::Rng; // 0.8.5
 
 pub mod levels;
 
 /// Size of the world and game grid
+pub const WORLD_OFFSET_OF_GRID: f32 = 5.0;
 pub const GRID_SIZE: f32 = 17.0;
-pub const WORLD_SIZE: f32 = GRID_SIZE + 4.0;
+pub const WORLD_SIZE: f32 = GRID_SIZE + 2.0 * WORLD_OFFSET_OF_GRID;
 
 pub const YELLOW: Color = Color::srgb(234.0 / 255.0, 189.0 / 255.0, 71.0 / 255.0);
 pub const MILESTONE_COLOR: Color = Color::srgb(155.0/255.0, 34.0/255.0, 38.0/255.0);
+pub const SCALE_COLOR: Color = Color::srgb(34.0/255.0, 34.0/255.0, 255.0/255.0);
 
 /// Component to identify the Corn
 #[derive(Component)]
@@ -34,14 +38,15 @@ fn setup_world(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
 
-    let FLOOR_SIZE = GRID_SIZE + 3.0;
+    let FLOOR_SIZE = GRID_SIZE + WORLD_OFFSET_OF_GRID;
+    let WORLD_OFFSET_OF_GRID2 = -WORLD_OFFSET_OF_GRID as isize;
     // This is the floor of the game, adding 2 tiles of margin
-    for x in -2..FLOOR_SIZE  as isize {
-        for z in -2..FLOOR_SIZE as isize {
+    for x in WORLD_OFFSET_OF_GRID2..FLOOR_SIZE  as isize {
+        for z in WORLD_OFFSET_OF_GRID2..FLOOR_SIZE as isize {
 
             let num = rand::thread_rng().gen_range(0.0..0.10);
 
-            if (x < 0) || (z < 0) || (z > GRID_SIZE as isize)|| (x > GRID_SIZE as isize)
+            if (x < 0) || (z < 0) || (z >= GRID_SIZE as isize) || (x >= GRID_SIZE as isize)
             {
                 commands.spawn((
                     PbrBundle {
@@ -67,19 +72,6 @@ fn setup_world(
         }
     }
 
-    // Sun
-    /*
-    commands.spawn(DirectionalLightBundle {
-        directional_light: DirectionalLight {
-            color: Color::srgb(0.98, 0.95, 0.82),
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(0.0, 0.0, 0.0)
-            .looking_at(Vec3::new(-0.15, -0.05, 0.25), Vec3::Y),
-        ..default()
-    });
-*/
     // 0 0 marker stone
     commands.spawn(PbrBundle {
         mesh: meshes.add(Cuboid::new(0.2, 2.0, 0.2)),
@@ -88,20 +80,25 @@ fn setup_world(
         ..default()
     });
 
-    // end of grid stone
     commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(0.2, 2.0, 0.2)),
-        material: materials.add(Color::srgb(0.0, 0.0, 1.0)),
-        transform: Transform::from_xyz(GRID_SIZE - 1.0, 1.0, GRID_SIZE - 1.0),
+        mesh: meshes.add(Cuboid::new(0.2, 1.0, 0.2)),
+        material: materials.add(SCALE_COLOR),
+        transform: Transform::from_xyz((GRID_SIZE - 1.0) / 2.0, 1.0, -1.0),
         ..default()
     });
 
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Cuboid::new(0.2, 1.0, 0.2)),
+        material: materials.add(SCALE_COLOR),
+        transform: Transform::from_xyz(-1.0, 1.0, (GRID_SIZE - 1.0) / 2.0),
+        ..default()
+    });
 
     // We need apparently to work on the X - Z plane, Y being the height for us.
     for x in 0..GRID_SIZE as usize {
         for z in 0..GRID_SIZE as usize {
 
-            let num = rand::thread_rng().gen_range(0.50..0.55);
+            let num = rand::thread_rng().gen_range(0.45..0.55);
 
             commands.spawn((
                 PbrBundle {
