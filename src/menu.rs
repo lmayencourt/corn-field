856fae8e96f506883d3f1274fdbf7525490e1002
Louis Mayencourt <louis.mayencourt@gmail.com>
2024-08-17 19:50:14 +0200

    Improve score and game state handling

--- src/menu.rs
+++ src/menu.rs
@@ -14,17 +14,34 @@ struct GameScore {
     mistakes: u32,
 }
 
+/// Component used to mark the mistakes at the end of the game
+#[derive(Component)]
+struct ScoreMarker;
+
 #[derive(Event, Default)]
 struct ComputeScoreEvent;
+
+/// Restart all the game element
+#[derive(Event, Default)]
+struct RestartGame;
+
+/// Old the previous input to provide a cool down to the enter key
+#[derive(Resource)]
+struct PreviousKeyboardInput {
+    previous_key: Option<KeyCode>,
+}
 
 pub struct MenuPlugin;
 
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, manage_menu);
+        app.insert_resource(PreviousKeyboardInput{previous_key: None});
         app.add_systems(Update, compute_score);
         app.add_event::<ComputeScoreEvent>();
         app.insert_resource(GameScore::default());
+        app.add_event::<RestartGame>();
+        app.add_systems(Update, restart_all);
     }
 }
 
@@ -33,6 +50,8 @@ fn manage_menu(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut next_state: ResMut<NextState<GameState>>,
     mut event: EventWriter<ComputeScoreEvent>,
+    mut restart: EventWriter<RestartGame>,
+    mut old_input: ResMut<PreviousKeyboardInput>,
 )
 {
     match state.get() {
@@ -42,18 +61,26 @@ fn manage_menu(
             }
         }
         GameState::InGame => {
-            if keyboard_input.pressed(KeyCode::Enter) {
+            if keyboard_input.pressed(KeyCode::Enter) && old_input.previous_key.is_none(){
                 next_state.set(GameState::EndGame);
+                old_input.previous_key = Some(KeyCode::Enter);
             }
         }
         GameState::EndGame => {
-            info!("End of the game, computing the score");
             event.send_default();
             next_state.set(GameState::Score);
         }
         GameState::Score => {
-
+            if keyboard_input.pressed(KeyCode::Enter) && old_input.previous_key.is_none(){
+                restart.send_default();
+                next_state.set(GameState::InGame);
+                old_input.previous_key = Some(KeyCode::Enter);
+            }
         }
+    }
+
+    if !keyboard_input.pressed(KeyCode::Enter) {
+        old_input.previous_key = None;
     }
 }
 
@@ -81,27 +108,43 @@ fn compute_score(
                     number_of_tile_to_clear += 1;
                     if field_map[x][y] == 1 {
                         score.forgotten += 1;
-                        commands.spawn(PbrBundle {
+                        commands.spawn((PbrBundle {
                             mesh: meshes.add(Cuboid::new(1.0, 0.2, 1.0)),
                             material: materials.add(Color::srgba(0.0, 0.0, 1.0, 0.4)),
                             transform: Transform::from_xyz(x as f32, 1.1, y as f32),
                             ..default()
-                        });
+                            },
+                            ScoreMarker
+                        ));
                     }
                 } else {
                     if field_map[x][y] == 0 {
                         score.mistakes += 1;
-                        commands.spawn(PbrBundle {
+                        commands.spawn((PbrBundle {
                             mesh: meshes.add(Cuboid::new(1.0, 0.2, 1.0)),
                             material: materials.add(Color::srgba(1.0, 0.0, 0.0, 0.4)),
                             transform: Transform::from_xyz(x as f32, 1.1, y as f32),
                             ..default()
-                        });
+                            },
+                            ScoreMarker
+                        ));
                     }
                 }
             }
         }
 
         info!("Score: {} forgotten, {} mistakes", score.forgotten, score.mistakes);
+    }
+}
+
+fn restart_all(
+    event: EventReader<RestartGame>,
+    mut commands: Commands,
+    markers: Query<Entity, With<ScoreMarker>>,
+) {
+    if !event.is_empty() {
+        for marker in markers.iter() {
+            commands.entity(marker).despawn();
+        }
     }
 }
