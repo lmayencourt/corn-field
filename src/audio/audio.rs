44ae96c74ba7bca595c1dd3605e8e74a4d0ea4a1
Florian Depraz <florian.depraz@outlook.com>
2024-08-17 10:30:01 +0200

    Audio
    Signed-off-by: Florian Depraz <florian.depraz@outlook.com>

---
+++ src/audio/audio.rs
@@ -0,0 +1,20 @@
+use bevy::prelude::*;
+
+pub struct AudioPlugin;
+
+impl Plugin for AudioPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup_sound);
+    }
+}
+
+fn setup_sound(asset_server: Res<AssetServer>, mut commands: Commands) {
+    commands.spawn(AudioBundle {
+        source: asset_server.load("sounds/game.ogg"),
+        settings: PlaybackSettings {
+            mode: bevy::audio::PlaybackMode::Loop,
+            ..default()
+        },
+        ..default()
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,20 @@ use bevy::prelude::*;
 
 mod camera;
 mod world;
+mod audio;
+mod player;
 
 use camera::GameCameraPlugin;
 use world::WorldPlugin;
+use audio::audio::AudioPlugin;
+use player::PlayerPlugin;
+
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(GameCameraPlugin)
         .add_plugins(WorldPlugin)
+        .add_plugins(AudioPlugin)
         .run();
 }
