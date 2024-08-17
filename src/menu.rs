318b61cf4eb088987b69085120e5c004a0a993b8
Louis Mayencourt <louis.mayencourt@gmail.com>
2024-08-17 19:12:24 +0200

    Improve score

--- src/menu.rs
+++ src/menu.rs
@@ -10,8 +10,8 @@ use crate::world::{Corn, YELLOW, GRID_SIZE, levels::LEVEL_1};
 /// Global resource that contains the score of the game
 #[derive(Resource, Default)]
 struct GameScore {
-    percent_correct: f32,
-    mistakes_nbr: u32,
+    forgotten: u32,
+    mistakes: u32,
 }
 
 #[derive(Event, Default)]
@@ -21,43 +21,11 @@ pub struct MenuPlugin;
 
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_menu);
         app.add_systems(Update, manage_menu);
         app.add_systems(Update, compute_score);
         app.add_event::<ComputeScoreEvent>();
         app.insert_resource(GameScore::default());
     }
-}
-
-fn setup_menu(mut commands: Commands) {
-    let text_style = TextStyle { ..default() };
-
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                width: Val::Percent(100.0),
-                height: Val::Percent(100.0),
-                flex_direction: FlexDirection::Column,
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                row_gap: Val::Px(text_style.font_size * 2.),
-                ..default()
-            },
-            ..default()
-        })
-        .with_children(|parent| {
-            parent.spawn(TextBundle::from_sections([
-                TextSection::new("press ".to_string(), text_style.clone()),
-                TextSection::new(
-                    "space".to_string(),
-                    TextStyle {
-                        color: YELLOW.into(),
-                        ..text_style.clone()
-                    },
-                ),
-                TextSection::new(" to mark the field".to_string(), text_style),
-            ]));
-        });
 }
 
 fn manage_menu(
@@ -103,31 +71,37 @@ fn compute_score(
             field_map[corn_position.translation.x as usize][corn_position.translation.z as usize] = 1;
         }
 
-        let mut correct_pos = 0.0;
+        score.mistakes = 0;
+        score.forgotten = 0;
+        let mut correct_pos = 0;
+        let mut number_of_tile_to_clear = 0;
         for x in 0..GRID_SIZE as usize {
             for y in 0..GRID_SIZE as usize {
-                if field_map[x][y] == LEVEL_1[x][y] {
-                    correct_pos += 1.0;
-                    commands.spawn(PbrBundle {
-                        mesh: meshes.add(Cuboid::new(1.0, 0.2, 1.0)),
-                        material: materials.add(Color::srgba(0.0, 0.0, 1.0, 0.4)),
-                        transform: Transform::from_xyz(x as f32, 1.1, y as f32),
-                        ..default()
-                    });
+                if LEVEL_1[x][y] == 0 {
+                    number_of_tile_to_clear += 1;
+                    if field_map[x][y] == 1 {
+                        score.forgotten += 1;
+                        commands.spawn(PbrBundle {
+                            mesh: meshes.add(Cuboid::new(1.0, 0.2, 1.0)),
+                            material: materials.add(Color::srgba(0.0, 0.0, 1.0, 0.4)),
+                            transform: Transform::from_xyz(x as f32, 1.1, y as f32),
+                            ..default()
+                        });
+                    }
                 } else {
-                    commands.spawn(PbrBundle {
-                        mesh: meshes.add(Cuboid::new(1.0, 0.2, 1.0)),
-                        material: materials.add(Color::srgba(1.0, 0.0, 0.0, 0.4)),
-                        transform: Transform::from_xyz(x as f32, 1.1, y as f32),
-                        ..default()
-                    });
-                    println!("x {}, y{} is different", x, y);
+                    if field_map[x][y] == 0 {
+                        score.mistakes += 1;
+                        commands.spawn(PbrBundle {
+                            mesh: meshes.add(Cuboid::new(1.0, 0.2, 1.0)),
+                            material: materials.add(Color::srgba(1.0, 0.0, 0.0, 0.4)),
+                            transform: Transform::from_xyz(x as f32, 1.1, y as f32),
+                            ..default()
+                        });
+                    }
                 }
             }
         }
 
-        score.percent_correct = correct_pos/(GRID_SIZE*GRID_SIZE) * 100.0;
-        score.mistakes_nbr = (GRID_SIZE*GRID_SIZE - correct_pos) as u32;
-        info!("Score is {}%, {} mistakes", score.percent_correct, score.mistakes_nbr);
+        info!("Score: {} forgotten, {} mistakes", score.forgotten, score.mistakes);
     }
 }
